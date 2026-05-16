50afd909b5b58024593d3c143d4988b433d5d8f1
RubizZ <45830465+RubizZ@users.noreply.github.com>
2026-05-17 01:11:44 +0200

    fix: Prisma ssl error on Vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,30 +3,35 @@ import { defineConfig } from "prisma/config";
 import * as dotenv from "dotenv";
 import path from "path";
 
-// Forzamos a dotenv a cargar el archivo .env de la raíz en local por si la CLI se despista
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
-// 1. Comprobamos primero si tenemos la variable local clásica (Desarrollo)
-let databaseUrl = process.env["DATABASE_URL"];
+// 1. Prioridad absoluta a nuestra variable espejo con esteroides SSL de Vercel
+let databaseUrl =
+    process.env["DATABASE_URL_WITH_SSL"] || process.env["DATABASE_URL"];
 
-// 2. Si no existe DATABASE_URL, significa que estamos en Vercel (Producción)
+// 2. Si no existen, es que estamos en el flujo de inicialización/integración estándar de Vercel
 if (!databaseUrl) {
-  const isPrismaCLI = process.argv.some(
-    (arg) => arg.includes("prisma") || arg.includes("db") || arg.includes("migrate")
-  );
+    const isPrismaCLI = process.argv.some(
+        (arg) =>
+            arg.includes("prisma") ||
+            arg.includes("db") ||
+            arg.includes("migrate"),
+    );
 
-  databaseUrl = isPrismaCLI
-    ? (process.env["POSTGRES_URL_NON_POOLING"] ?? process.env["POSTGRES_PRISMA_URL"])
-    : (process.env["POSTGRES_PRISMA_URL"] ?? process.env["POSTGRES_URL_NON_POOLING"]);
+    databaseUrl = isPrismaCLI
+        ? (process.env["POSTGRES_URL_NON_POOLING"] ??
+          process.env["POSTGRES_PRISMA_URL"])
+        : (process.env["POSTGRES_PRISMA_URL"] ??
+          process.env["POSTGRES_URL_NON_POOLING"]);
 }
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-    seed: 'npx tsx prisma/seed.ts',
-  },
-  datasource: {
-    url: databaseUrl,
-  },
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+        seed: "npx tsx prisma/seed.ts",
+    },
+    datasource: {
+        url: databaseUrl,
+    },
 });
